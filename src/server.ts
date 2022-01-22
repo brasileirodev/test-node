766cf5b602271bfbf53c63750757737045e31331
Marcel Brasileiro <brasileirodev@gmail.com>
2022-01-22 14:25:05 -0300

    Config Eslint

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,20 @@ import express from 'express';
 
 const app = express();
 
- app.use(express.json());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'GoStack' });
-});
+app.get('/', (req, res) => res.json({ message: 'GoStack' }));
 
 app.post('/users', (req, res) => {
-    const { name, email } = req.body;
+  const { name, email } = req.body;
 
-    const userTemp = {
-        name,
-        email
-    };
-    return res.status(201).json(userTemp);
-})
+  const userTemp = {
+    name,
+    email,
+  };
+  return res.status(201).json(userTemp);
+});
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333! 🚀');
+  console.log('Server started on port 3333! 🚀');
 });
